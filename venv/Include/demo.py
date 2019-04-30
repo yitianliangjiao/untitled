d58c090d8aca82464c1ad547d80134f4e7faffdb
yitianliangjiao <wrh@163.com>
2019-04-30 22:36:22 +0800

    xpath解析

--- venv/Include/demo.py
+++ venv/Include/demo.py
@@ -1,5 +1,12 @@
-from DBUtil import  DbBean
-o = DbBean()
-testdict = {"procode":"2019-130522-34-03-000018"}
+import urllib.request
+from lxml import etree
 
-o.add(testdict,"t_pro")
+params = {'sortname': 'BGTIME', 'sortorder': 'sortorder', 'xzqh': '130000', 'query': '', 'rp': '15', 'page': '1'}
+data = bytes(urllib.parse.urlencode(params), encoding='utf8')
+print(data)
+response = urllib.request.urlopen('http://tzxm.hbzwfw.gov.cn/tzxmweb/main/gsxx', data=data)
+response = response.read().decode('utf-8')
+html = etree.HTML(response)
+#html_data = html.xpath("//*[@id='listForm']/table/tr/td[3]/table[2]/tbody/tr[2]/td[2]")
+html_data = html.xpath("//*[@id='listForm']/table")
+print (html_data)
