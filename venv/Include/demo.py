3bf35b43df02b794f929216ce83d7a7016a5efbe
yitianliangjiao <wrh@163.com>
2019-04-30 22:43:31 +0800

    xpath解析

--- venv/Include/demo.py
+++ venv/Include/demo.py
@@ -8,5 +8,5 @@ response = urllib.request.urlopen('http://tzxm.hbzwfw.gov.cn/tzxmweb/main/gsxx',
 response = response.read().decode('utf-8')
 html = etree.HTML(response)
 #html_data = html.xpath("//*[@id='listForm']/table/tr/td[3]/table[2]/tbody/tr[2]/td[2]")
-html_data = html.xpath("//*[@id='listForm']/table")
+html_data = html.xpath("//*[@id='listForm']//table[@class='ej_zjfw_list']//td//@title")
 print (html_data)
