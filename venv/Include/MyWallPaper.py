7e6dce954098b5932eefa536e14f364cd1e08dec
yitianliangjiao <wrh@163.com>
2019-05-17 12:48:24 +0800

    设置墙纸

--- venv/Include/MyWallPaper.py
+++ venv/Include/MyWallPaper.py
@@ -1,8 +1,11 @@
 import requests
 from lxml import etree
 import time
+import os
+import win32gui,win32con,win32api
 remoteUrl = 'https://cn.bing.com/'
 UserAgent = 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36'
+imgpath = 'E:\wallPaper\%s.jpg'
 
 
 def get_img_url():
@@ -24,8 +27,15 @@ def getfilefromurl(fileurl):
             'User-Agent': UserAgent})
     local = time.strftime("%Y.%m.%d")
     print(local)
-    f = open('E:\wallPaper\%s.jpg' % '555', 'wb')
+    f = open(imgpath % local, 'wb')
     f.write(r.content)
     f.close()
-getfilefromurl(get_img_url())
+def setWallPaper(filepath):
+    k = win32api.RegOpenKeyEx(win32con.HKEY_CURRENT_USER, "Control Panel\\Desktop", 0, win32con.KEY_SET_VALUE)
+    win32api.RegSetValueEx(k, "WallpaperStyle", 0, win32con.REG_SZ, "2")  # 2拉伸适应桌面，0桌面居中
+    win32api.RegSetValueEx(k, "TileWallpaper", 0, win32con.REG_SZ, "0")
+    win32gui.SystemParametersInfo(win32con.SPI_SETDESKWALLPAPER, filepath, 1 + 2)
+
+local = time.strftime("%Y.%m.%d")
+setWallPaper(imgpath % local)
 
