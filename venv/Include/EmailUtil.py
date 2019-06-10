cb30fca15ee0b58cc6a158416e83912106577437
yitianliangjiao <wrh@163.com>
2019-06-10 18:25:34 +0800

    邮件

--- venv/Include/EmailUtil.py
+++ venv/Include/EmailUtil.py
@@ -12,13 +12,20 @@ class DbBean:
         self.myEmailAccount = conf.get('myemail', 'myEmailAccount')
         self.myEmailPassword = conf.get('myemail', 'myEmailPassword')
 
-    def sendEmail(self,sender,receivers,msg):
+    def sendEmail(self,sender,receivers,msg,subject):
         try:
-            print(self.myEmailHost)
+            cc = sender
+            toaddrs = [receivers] + [cc]
+            print(toaddrs)
+            message = MIMEText(msg, 'plain', 'utf-8')
+            message['From'] = "<%s>" % sender
+            message['To'] = "<%s>" % receivers
+            message['Subject'] = Header(subject, 'utf-8')
+            message['Cc'] = "<%s>"  % sender
             smtpObj = smtplib.SMTP(self.myEmailHost,25)
             smtpObj.set_debuglevel(1)
             smtpObj.login(self.myEmailAccount, self.myEmailPassword)
-            smtpObj.sendmail(sender, receivers, message.as_string())
+            smtpObj.sendmail(sender, toaddrs, message.as_string())
             smtpObj.quit()
             print("邮件发送成功")
         except smtplib.SMTPException:
@@ -27,17 +34,8 @@ class DbBean:
 
 sender = '15732136845@163.com'
 receivers = '1945623989@qq.com'  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
-cc = sender
-toaddrs = [receivers]+[cc]
-print(toaddrs)
-message = MIMEText('撒打算打算打所三三四四萨大苏打发我发誓分散.', 'plain', 'utf-8')
-message['From'] = "<15732136845@163.com>"
-message['To'] = "<1945623989@qq.com>"
 subject = '抓紧参加这次会议'
-message['Subject'] = Header(subject, 'utf-8')
-message['Cc'] = "<15732136845@163.com>"
-print(message.as_string())
 d = DbBean()
-d.sendEmail(sender,toaddrs,message)
+d.sendEmail(sender,receivers,message,subject)
 
 
